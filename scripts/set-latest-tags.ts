068d20b1f4bdba97234f2ee450e70fecb39e16be
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-02-09 21:03:00 +0500

    chore: Add `set-latest-tags` script.

--- scripts/set-latest-tags.ts
+++ scripts/set-latest-tags.ts
@@ -1,22 +1,21 @@
-import { exec } from '@actions/exec';
-
-import { version } from '../lerna.json';
+import { exec } from "@actions/exec";
+import { version } from "../package.json";
 
 async function main() {
-  const [major, minor] = version.split('.');
+  const [major, minor] = version.split(".");
   const tag = `v${version}`;
   const latestTags = [`v${major}`, `v${major}.${minor}`];
 
   try {
-    await exec('git', ['push', '--delete', 'origin', ...latestTags], {
+    await exec("git", ["push", "--delete", "origin", ...latestTags], {
       failOnStdErr: false,
     });
   } finally {
     for (const latestTag of latestTags) {
-      await exec('git', ['tag', '--force', latestTag, tag]);
+      await exec("git", ["tag", "--force", latestTag, tag]);
     }
 
-    await exec('git', ['push', 'origin', '--tags']);
+    await exec("git", ["push", "origin", "--tags"]);
   }
 }
 
