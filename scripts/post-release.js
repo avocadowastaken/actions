53c937622e366a452f165e94154f9faea910b299
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-10-11 11:27:51 +0300

    feat: Remove obsolete utils.

--- scripts/post-release.js
+++ scripts/post-release.js
@@ -1,5 +1,3 @@
-"use strict";
-
 import execa from "execa";
 import Listr from "listr";
 
--- utils/log.ts
+++
@@ -1,6 +0,0 @@
-import { info } from "@actions/core";
-import * as util from "util";
-
-export function logInfo(format: string, ...args: unknown[]): void {
-  info(util.format(format, ...args));
-}
